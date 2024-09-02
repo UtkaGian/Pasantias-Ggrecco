e8f18681e262e3ed7a75f06a0bddc9dbdda764ee
Streaming Center 2 <sc2@etrr.edu.ar>
2024-09-02 12:15:35 -0300

    Se logra ejecutar la parte de los movimientos, queda hacer una cunción que cargue varios

--- FrontieRobot/app/src/main/java/com/example/frontierobot/ui/theme/MovingRobot.kt
+++ FrontieRobot/app/src/main/java/com/example/frontierobot/ui/theme/MovingRobot.kt
@@ -2,8 +2,11 @@ package com.example.frontierobot.ui.theme
 
 import android.content.res.Resources.Theme
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.frontierobot.R
@@ -13,6 +16,20 @@ class MovingRobot : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_moving_robot)
+        val coordx= intent.extras?.getInt("CoordX")
+        val coordy= intent.extras?.getInt("CoordY")
+        val Ordis= findViewById<ImageView>(R.id.Ordis)
+        val params= Ordis.layoutParams as RelativeLayout.LayoutParams
+        if (coordx != null) {
+            params.leftMargin= coordx
+        }
+        if (coordy != null) {
+            params.topMargin= coordy
+        }
+        Ordis.layoutParams= params
+    }
+}
+/*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,3 +37,5 @@ class MovingRobot : AppCompatActivity() {
         }
     }
 }
+
+ */
