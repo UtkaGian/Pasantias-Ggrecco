ce4ec607fd177852e3e28ef14969edb6c5fc5717
Streaming Center 2 <sc2@etrr.edu.ar>
2024-09-02 09:50:42 -0300

    Se sigue peleando con el front end

---
+++ FrontieRobot/app/src/main/java/com/example/frontierobot/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.frontierobot
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.frontierobot.ui.theme.FrontieRobotTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_init)
+        
+    }
+}
+
+
+
+/*
+        enableEdgeToEdge()
+        setContent {
+            FrontieRobotTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FrontieRobotTheme {
+        Greeting("Android")
+    }
+}
+
+ */
